9af771b094eeb39f032dcdddf31f1e004df669da
a345163745 <0oalexlio0@gmail.com>
2022-06-27 17:34:27 -0400

    test

--- server/index.js
+++ server/index.js
@@ -8,13 +8,16 @@ app.use(cors());
 app.use(router)
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
-
+/*
 const io = new Server(server,{
     cors:{
         origin:"http://localhost:3000", //where we get info from
         methods:["GET","POST"], //what method we accept
     }
 })
+*/
+const io = socketio(server);
+app.use(cors());
 
 io.on("connection",(socket)=>{
     console.log(`User Connected ${socket.id}`);
